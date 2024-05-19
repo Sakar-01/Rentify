8eae5b5f4ab2131816b2a95426564006c818c5f2
Sakar Vaidya <sakarvaidya72527@gmail.om>
2024-05-19 19:13:26 +0530

    Auth Backend ref:https://github.com/Sakar-01/Secure-File-Storage-and-Sharing

---
+++ Backend/src/routes/users-routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  getAllUsers,
+  UserLogin,
+  UserSignup,
+  verifyUser,
+  userLogout
+} from "../controllers/usersControllers.js";
+import {userSignupValidator,validate,loginValidator} from "../utils/validators.js"
+import { verifyToken } from "../utils/token-manager.js";
+
+const userRoutes = express.Router();
+
+userRoutes.get("/getallUsers", getAllUsers);
+userRoutes.post("/signup",  validate(userSignupValidator), UserSignup);
+userRoutes.post("/login", validate(loginValidator), UserLogin);
+userRoutes.get("/verify", verifyToken, verifyUser);
+userRoutes.get("/logout", verifyToken, userLogout);
+
+
+export default userRoutes;
